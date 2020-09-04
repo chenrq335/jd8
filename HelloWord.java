7537acb75f2833bdb905cc218be1220ddde191f8
chenrq335 <43854955+chenrq335@users.noreply.github.com>
2020-09-04 14:18:07 +0800

    second Update1
    second Update1

--- HelloWord.java
+++ HelloWord.java
@@ -3,7 +3,7 @@ package com.szy.entity;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World£¡1111");
+		System.out.println("Hello World£¡2222");
 	}
 
  }
